a78d3bd7e9090d381bf22abc8aaa008dd9b2ab09
Priyanka M <162833693+codermpriyanka@users.noreply.github.com>
2024-03-16 17:00:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let username=document.getElementById("username");
+let password=document.getElementById("password");
+let flag=1;
+function validateForm(){
+    if (username.value==""){
+        document.getElementById("userError").innerHTML="Username is Empty"
+        flag=0;
+    }
+    else if(username.value.length<3){
+        document.getElementById("userError").innerHTML="User name must be more than 3 character"
+        flag=0;
+    }
+    else{
+        document.getElementById("userError").innerHTML="";
+        flag=1;
+    }
+    if (password.value==""){
+        document.getElementById("passerror").innerHTML="Password is Empty"
+        flag=0;
+    }
+    else{
+        document.getElementById("passError").innerHTML="";
+        flag=1;
+    }
+    if(flag){
+        return true;
+    }else{
+        return false;
+    }
+    
+    
+
+    
+    return false;
+}
